aabb348ea6dc4cbbd251e61757cb37985a918998
Phillip Boateng <cnolimit@gmail.com>
2019-09-14 12:33:42 +0100

    setup project with apollo react

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ListView from "./components/ListView/list-view.container";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "http://localhost:4567/graphql"
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => (
+  <ApolloProvider client={client}>
+    <ListView />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
